aace809790b9cf34a7d0ddd92f4dd7248fdda069
jeevith-deloitte <jravikumar@deloitte.com>
2022-06-01 12:51:15 +0530

    Models Created

---
+++ Models/Player.java
@@ -0,0 +1,20 @@
+
+import java.util.UUID;
+
+public class Player{
+    private String name;
+    private String id;
+
+    public Player(String name){
+        this.name=name;
+        this.id= UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Models/Snake.java
@@ -0,0 +1,17 @@
+public class Snake{
+    private int start;
+    private int end;
+
+    Snake(int start,int end){
+        this.start=start;
+        this.end=end;
+    }
+
+    int getStart(){
+        return start;
+    }
+
+    int getEnd(){
+        return end;
+    }
+}
